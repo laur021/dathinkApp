e5e3132573cfa3ddbbe79de48874642c097f411d
laur021 <laurence.bacolor@gmail.com>
2024-11-06 13:52:58 +0800

    Setup Connection

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578d3a4854510e2356136a2e61c63022ef9ff6b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92adb8018939ea3f6948cb98babbb89a99b539c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
