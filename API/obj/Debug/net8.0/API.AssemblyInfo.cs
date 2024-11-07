c562a63389f457df3895911a72938f507eef9b75
laur021 <laurence.bacolor@gmail.com>
2024-11-07 09:21:17 +0800

    Setup API Skeleton

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92adb8018939ea3f6948cb98babbb89a99b539c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e3132573cfa3ddbbe79de48874642c097f411d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
