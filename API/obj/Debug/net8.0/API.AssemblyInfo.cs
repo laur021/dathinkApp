f09aaf702bb4e8d42841c7377f88bc26480315b3
laur021 <laurence.bacolor@gmail.com>
2024-11-07 13:59:17 +0800

    setup client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e3132573cfa3ddbbe79de48874642c097f411d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102a2de7872412f3f73631a43a17054b4b356276")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
