56735b0367b40697eabda29b95e982f7685c6bf0
laur021 <laurence.bacolor@gmail.com>
2024-11-11 08:19:12 +0800

    "fixed files"

--- API/DTOs/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace API.DTOs;
-
-public class LoginDto
-{
- public required string Username { get; set; }
- public required string Password { get; set; }
-}
--- API/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using API.Entities;
-
-namespace API.Interfaces;
-
-public interface ITokenService
-{
-    string CreateToken(AppUser user);
-}
