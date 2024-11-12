f435de84b799a0168e98cc7ee8d2d96a3281e03f
laur021 <laurence.bacolor@gmail.com>
2024-11-12 08:31:56 +0800

    Add components and routing path

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'members', component: MemberListComponent},
+    {path: 'members/:id', component: MemberDetailComponent},
+    {path: 'lists', component: ListsComponent},
+    {path: '**', component: HomeComponent, pathMatch: 'full'}, //if no link match it directs to HomeComponent
+];
