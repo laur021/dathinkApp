2c049eabf750d4105a8833d7662ff561ddfe16a0
laur021 <laurence.bacolor@gmail.com>
2024-11-19 08:53:02 +0800

    Add environment

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,46 +1,47 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { map } from 'rxjs';
-import { User } from '../_models/user';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+import { map } from "rxjs";
+import { User } from "../_models/user";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AccountService {
   // variables
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
   //ctor
   constructor() {}
 
-
   login(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map(user => { //user variable get the response(user details) from API
-        if(user){
-          localStorage.setItem('user', JSON.stringify(user)); //goes to localstorage as keyvalue pair
+    return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
+      map((user) => {
+        //user variable get the response(user details) from API
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user)); //goes to localstorage as keyvalue pair
           this.currentUser.set(user);
         }
       })
-    )
+    );
   }
 
   register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map(user => {
-        if(user){
-          localStorage.setItem('user', JSON.stringify(user));
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
           this.currentUser.set(user);
         }
         return user;
       })
-    )
+    );
   }
 
   logout() {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
     this.currentUser.set(null);
   }
 }
