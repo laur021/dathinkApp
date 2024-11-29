92549990fad12c93012449af65e6e59e6d725408
laur0211 <laur.bacolor@gmail.com>
2024-11-29 20:00:18 +0800

    add deactive route

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,20 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  OnInit,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { KeyValuePipe, NgFor, NgIf } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
   standalone: true,
-  imports: [NgFor, NgIf, KeyValuePipe],
+  imports: [TabsModule, NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css',
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm; //access #editForm="ngForm" from html
+  @HostListener('window:beforeunload', ['$event']) notify($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.loadMember();
@@ -26,5 +43,11 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(user.username).subscribe({
       next: (member) => (this.member = member),
     });
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
